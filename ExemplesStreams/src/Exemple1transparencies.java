4edfbef8efc9b06a5b1fbcbe81cd433a176d236c
Gerard <gerard.gee.2@gmail.com>
2015-11-20 16:12:26 +0100

    Exemples fitxers

---
+++ ExemplesStreams/src/Exemple1transparencies.java
@@ -0,0 +1,10 @@
+class Exemple1transparencies {
+ public static void main(String[] args){
+   int count = 0;
+   try {
+      while (System.in.read() !=-1)
+          count++;
+   }catch (java.io.IOException e) {}
+   System.out.println("L'entrada te " + count + " caracters.");
+ }
+}
